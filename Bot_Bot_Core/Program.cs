1ad3c656dc097b7cbef68baf3dbb97a8943efe06
asd7766zxc <42734628+asd7766zxc@users.noreply.github.com>
2018-09-09 13:20:10 +0800

    first commit

---
+++ Bot_Bot_Core/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bot_Bot_Core
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Core-Discord-Bot-
